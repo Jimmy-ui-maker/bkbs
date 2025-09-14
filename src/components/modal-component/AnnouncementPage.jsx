5b3a3fadd6f0e9acdf9254ce3f9e0c0d03230b94
Jimmy Yakubu <jimiyaks3@gmail.com>
2025-09-15 00:03:07 +0100

    bkbs sss

--- src/components/modal-component/AnnouncementPage.jsx
+++ src/components/modal-component/AnnouncementPage.jsx
@@ -5,20 +5,26 @@ import AnnouncementCard from "./AnnouncementCard";
 export default function AnnouncementPage() {
   const announcements = [
     {
-      author: "Mrs. Johnson",
+      author: "Mrs. Omotosho Fumilayo",
       role: "Head Teacher",
       time: "Sep 14, 2025 - 10:30 AM",
       body: "The mid-term exams will begin next week. Please ensure all students are prepared.",
     },
     {
-      author: "Mr. Smith",
+      author: "Mrs Adeola Nathaniel",
+      role: "Administrator",
+      time: "Sep 14, 2025 - 10:30 AM",
+      body: "The school will be closing by 12PM every Friday starting from next week while extracurricular continues till 1PM.",
+    },
+    {
+      author: "Mr. Jimmy Yakubu",
       role: "Math Teacher",
       time: "Sep 13, 2025 - 2:00 PM",
       body: "Extra math classes will be held after school hours for Grade 6 students.",
     },
     {
-      author: "School Admin",
-      role: "Administrator",
+      author: "Mr Adeojo Nathaniel ",
+      role: "Director",
       time: "Sep 12, 2025 - 9:15 AM",
       body: "Reminder: School uniforms must be worn on all school days.",
     },
